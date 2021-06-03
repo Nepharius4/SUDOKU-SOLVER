e7889632fe73b39d388d1ae5c398e738464293a6
Nepharius4 <85313597+Nepharius4@users.noreply.github.com>
2021-06-03 22:28:31 +0300

    Create color.c

---
+++ color.c
@@ -0,0 +1,18 @@
+#include “sudokulib.h”
+//Changing colours for texts
+void red()
+{
+printf(“\033[1;31m”);
+}
+void cyan()
+{
+printf(“\033[0;36m”);
+}
+void standart()
+{
+printf(“\033[0m”);
+}
+void green()
+{
+printf(“\033[0;32m”);
+}
